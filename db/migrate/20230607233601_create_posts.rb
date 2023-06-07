3217a0b6f51eaf41e9ba0ed572fffdfb551132ba
Caren-Koroeny <carensiya5@gmail.com>
2023-06-08 02:41:30 +0300

    Removed duplicated lines of create table

--- db/migrate/20230607233601_create_posts.rb
+++ db/migrate/20230607233601_create_posts.rb
@@ -1,14 +1,13 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      create_table :posts do |t|
-        t.integer :AuthorId
-        t.string :Title
-        t.string :Text
-        t.string :CreatedAt
-        t.string :UpdatedAt
-        t.integer :CommentsCounter
-        t.integer :LikesCounter
+      t.integer :AuthorId
+      t.string :Title
+      t.string :Text
+      t.string :CreatedAt
+      t.string :UpdatedAt
+      t.integer :CommentsCounter
+      t.integer :LikesCounter
 
       t.timestamps
     end
