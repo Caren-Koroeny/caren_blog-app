714668a5e0c300016aadea06e89789dc6be01cc7
Caren-Koroeny <carensiya5@gmail.com>
2023-06-08 14:01:43 +0300

    Add a foreign key column authorId to the posts

--- db/migrate/20230608001258_add_user_reference_to_post.rb
+++ db/migrate/20230608001258_add_user_reference_to_post.rb
@@ -1,6 +1,5 @@
 class AddUserReferenceToPost < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :user, null: false, foreign_key: true
-    t.references :author, foreign_key: { to_table: 'users' }
+    add_reference :posts, :author, null: false, foreign_key: { to_table: 'users' }
   end
 end
