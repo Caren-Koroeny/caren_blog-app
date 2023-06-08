e616b1c8ed45549da3c2cb64a714b186c9b7243d
Caren-Koroeny <carensiya5@gmail.com>
2023-06-08 13:02:04 +0300

    Made authorId as a foreign key in tables

--- db/migrate/20230608001258_add_user_reference_to_post.rb
+++ db/migrate/20230608001258_add_user_reference_to_post.rb
@@ -1,5 +1,6 @@
 class AddUserReferenceToPost < ActiveRecord::Migration[7.0]
   def change
     add_reference :posts, :user, null: false, foreign_key: true
+    t.references :author, foreign_key: { to_table: 'users' }
   end
 end
--- db/migrate/20230608001634_add_post_ref_to_comment.rb
+++ db/migrate/20230608001634_add_post_ref_to_comment.rb
@@ -1,5 +1,6 @@
 class AddPostRefToComment < ActiveRecord::Migration[7.0]
   def change
     add_reference :comments, :post, null: false, foreign_key: true
+    t.references :author, foreign_key: { to_table: 'users' }
   end
 end
