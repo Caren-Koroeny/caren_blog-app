54d098ca4154f05d08f8362834fcc4dd90ecbc58
Caren-Koroeny <carensiya5@gmail.com>
2023-06-09 18:33:37 +0300

    Implement after_save callback is added to the Like model.

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,9 +2,11 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  def self.update(id)
-    Post.find(id).increment!(:likes_counter)
+  after_save :update_likes_counter
+
+  private
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
   end
 end
-
-
