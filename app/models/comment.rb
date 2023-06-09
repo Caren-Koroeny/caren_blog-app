98e0a98aa3994a15a08e7e762cc3df55034810fb
Caren-Koroeny <carensiya5@gmail.com>
2023-06-09 18:36:56 +0300

    Implement after_save callback is added to the Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,13 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post
-
-  def self.update(id)
-    Post.find(id).increment!(:comments_counter)
+    belongs_to :author, class_name: 'User'
+    belongs_to :post
+  
+    after_save :update_comments_counter
+  
+    private
+  
+    def update_comments_counter
+      post.increment!(:comments_counter)
+    end
   end
-end
+  
