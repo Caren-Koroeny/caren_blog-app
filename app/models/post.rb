c46b8ec0b638ec47d602b343e829257194110712
Caren-Koroeny <carensiya5@gmail.com>
2023-06-10 19:08:20 +0300

    Implemented Post test

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,14 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_save :update_post_counter
 
-  def recent_comments(limit = 5)
-    Comment.order(created_at: :desc).limit(limit)
+  def update_posts_counter
+    author.increment!(:posts_counter)
   end
 
   def last_five_comments
