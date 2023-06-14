740549449a2c7d10010010396c8b70898bb85cef
Caren-Koroeny <carensiya5@gmail.com>
2023-06-14 16:09:04 +0300

    Added test for users and posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'renders the index template and includes correct placeholder text' do
+      get '/users/:user_id/posts'
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:index)
+      expect(response.body).to include('A list of all posts for a given user')
+    end
+  end
+
+  describe 'GET /show' do
+    it 'renders the show template and includes correct placeholder text' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:show)
+      expect(response.body).to include('Details for a particular post of a given user')
+    end
   end
 end
+
+
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /users' do
+    it 'renders the index template and includes correct placeholder text' do
+      # make get request
+      get '/users'
+      # check response body/status
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:index)
+      expect(response.body).to include('This page lists all the registered users')
+    end
+  end
+  describe 'GET /show' do
+    it 'renders the show template and includes correct placeholder text' do
+      get '/users/:id'
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:show)
+      expect(response.body).to include('This page shows the page for a given logged in user')
+    end
   end
 end
