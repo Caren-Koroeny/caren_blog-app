291d2920ade41d5f368afd76db4691042f1e8c36
Caren-Koroeny <carensiya5@gmail.com>
2023-06-16 02:39:27 +0300

    Number of post appear

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,16 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @user_posts = @user.posts.order(created_at: :desc)
+    @recent_comments = @user_posts.map(&:recent_five_comments).flatten
+  end
 
-  def show; end
+
+  def show
+    @user = User.find(params[:user_id])
+    @user_post = @user.posts.find(params[:id])
+    @post_index = @user.posts.order(created_at: :desc).pluck(:id).index(@user_post.id) + 1
+    @likes_count = @user_post.likes_counter
+    @comments_count = @user_post.comments_counter
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :comments
-  has_many :likes
+  belongs_to :author, class_name: 'User', dependent: :destroy, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'post_id'
+  has_many :likes, foreign_key: 'post_id'
+  after_save :update_posts_counter
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :commentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
