fb3c78f7e39bb50689f003126f96b65fd749f15f
Caren-Koroeny <carensiya5@gmail.com>
2023-06-17 22:27:47 +0300

    Fix linters error

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,18 +1,19 @@
 class CommentsController < ApplicationController
-    def new
-      @comment = Comment.new
+  def new
+    @comment = Comment.new
+  end
+
+  def create_comment
+    @user_post = Post.find(params[:id])
+    @comment = @user_post.comments.build(comment_params)
+    @comment.author = current_user
+
+    if @comment.save
+      redirect_to user_post_path(user_id: @user_post.author_id, id: @user_post.id),
+                  notice: 'Comment created successfully.'
+    else
+      redirect_to user_post_path(user_id: @user_post.user.id, id: @user_post.id),
+                  alert: 'Failed to create comment.'
     end
-    def create_comment
-        @user_post = Post.find(params[:id])
-        @comment = @user_post.comments.build(comment_params)
-        @comment.author = current_user
-    
-        if @comment.save
-          redirect_to user_post_path(user_id: @user_post.author_id, id: @user_post.id),
-                      notice: 'Comment created successfully.'
-        else
-          redirect_to user_post_path(user_id: @user_post.user.id, id: @user_post.id),
-                      alert: 'Failed to create comment.'
-        end
-      end
-    
+  end
+end
