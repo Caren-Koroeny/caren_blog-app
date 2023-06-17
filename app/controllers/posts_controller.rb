8a45626689fe53eb2903ad8ee8626e852854ab03
Caren-Koroeny <carensiya5@gmail.com>
2023-06-17 18:00:06 +0300

    Added new.html file logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,46 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @user_post = @user.posts.find(params[:id])
+    @author_id = current_user.id
     @post_index = @user.posts.order(created_at: :desc).pluck(:id).index(@user_post.id) + 1
     @likes_count = @user_post.likes_counter
     @comments_count = @user_post.comments_counter
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to user_posts_url(user_id: @post.author_id)
+    else
+      render :new
+    end
+  end
+
+  def create_comment
+    @user_post = Post.find(params[:id])
+    @comment = @user_post.comments.build(comment_params)
+    @comment.author = current_user
+
+    if @comment.save
+      redirect_to user_post_path(user_id: @user_post.author_id, id: @user_post.id), notice: 'Comment created successfully.'
+    else
+      redirect_to user_post_path(user_id: @user_post.user.id, id: @user_post.id), alert: 'Failed to create comment.'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :text)
   end
 end
---
+++ app/views/posts/new.html.rb
@@ -0,0 +1,27 @@
+<%= form_with(model: @post, url: user_posts_path(user_id: current_user.id), class: 'post-form') do |form| %>
+    <h2>Add User Post</h2>
+    <div class="form-group">
+      <%= form.label :title %>
+      <%= form.text_field :title, placeholder: 'Enter your title', class: 'form-control' %>
+    </div>
+  
+    <div class="form-group">
+      <%= form.label :text %>
+      <%= form.text_field :text, placeholder: 'Enter your text', class: 'form-control' %>
+    </div>
+  
+    <div class="form-group">
+      <%= form.label :comments_counter %>
+      <%= form.number_field :comments_counter, placeholder: 'Enter number of comments', class: 'form-control' %>
+    </div>
+  
+    <div class="form-group">
+      <%= form.label :likes_counter %>
+      <%= form.number_field :likes_counter, placeholder: 'Enter number of likes', class: 'form-control' %>
+    </div>
+  
+    <div class="form-group">
+      <%= form.submit "Create Post", class: 'btn btn-primary' %>
+    </div>
+  <% end %>
+  
