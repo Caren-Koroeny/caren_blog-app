2078ed3cf5ea80e87652d5154f25b9ec5b22f9fd
Caren-Koroeny <carensiya5@gmail.com>
2023-06-21 17:07:40 +0300

    Fix linters errors

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -3,8 +3,11 @@ require 'capybara/rspec'
 RSpec.describe 'User Index Page', type: :feature do
   before do
     # Create sample users and posts
-    @user1 = User.create!(name: 'User 1', photo: 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png', posts_counter: 12)
-    @user2 = User.create!(name: 'User 2', photo: 'https://cdn-icons-png.flaticon.com/512/21/21104.png', posts_counter: 130)
+    @user1 = User.create!(name: 'User 1',
+                          photo: 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
+                          posts_counter: 12)
+    @user2 = User.create!(name: 'User 2', photo: 'https://cdn-icons-png.flaticon.com/512/21/21104.png',
+                          posts_counter: 130)
     @user1.posts.create!(title: 'Post 1', text: 'Text for Post 1')
     @user2.posts.create!(title: 'Post 2', text: 'Text for Post 2')
   end
