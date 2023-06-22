16a31f977017c0f763282c8b42903253e0b6c7b3
Caren-Koroeny <carensiya5@gmail.com>
2023-06-22 04:52:16 +0300

    Set up the default URL options for the Devise mailer in each environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   get 'users', to: 'users#index'
   get 'users/:id', to: 'users#show', as: 'user'
   get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
