fa39ebb827593608182b219f6e854e8e3247282c
Caren-Koroeny <carensiya5@gmail.com>
2023-06-24 23:10:55 +0300

    Add more field to the schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  validates :name, :email, presence: true
+  validates :name, :email, presence: true, uniqueness: true
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
--- db/migrate/20230624173028_change_default_photo.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDefaultPhoto < ActiveRecord::Migration[7.0]
-  def change
-    change_column :users, :photo, :string, :default => 'https://source.unsplash.com/random/900x700/?user'
-  end
-end
